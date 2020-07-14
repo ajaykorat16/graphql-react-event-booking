43f4b4527b168b513e6ba6b86cef741e3b2f4a73
Ajay Korat <ajaykorat16@gmail.com>
2020-07-14 20:11:04 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', 
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+
+            type RootMutation {
+                createEvent(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events : () => {
+                return ['Romatic', 'Loving', 'Late night music']
+            },
+            createEvent : (args) => {
+                const eventsName = args.name;
+                return eventsName;
+            }
+        },
+        graphiql:true
+    }
+))
+
+app.listen(3000);
