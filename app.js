f353651be9c59ac69ac832ec492f1d200f820087
Ajay Korat <ajaykorat16@gmail.com>
2020-07-26 10:36:33 +0530

    manage events in temp memory

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
-
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
 app.use('/graphql', 
     graphqlHTTP({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
 
             schema {
@@ -25,11 +41,19 @@ app.use('/graphql',
         `),
         rootValue: {
             events : () => {
-                return ['Romatic', 'Loving', 'Late night music']
+                return events;
             },
             createEvent : (args) => {
-                const eventsName = args.name;
-                return eventsName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                }
+                events.push(event);
+
+                return event;
             }
         },
         graphiql:true
